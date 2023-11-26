67056c1c318ff5129b5338ca0983dd9f97d4b9b9
bawrer <zwenibonginkosi@gmail.com>
2023-11-26 10:53:01 +0200

    SAVE

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
- We need to work on js only
+document.addEventListener('DOMContentLoaded', function () {
+    const inputElement = document.querySelector('.input');
+    const outputElement = document.querySelector('.output');
+
+    document.querySelector('.keys').addEventListener('click', function (event) {
+       const target = event.target.closest('.key');
+       if (!target) return;
+
+       const keyType = target.dataset.key;
+       const keyText = target.innerText;
+
+       if (keyType === 'clear') {
+          inputElement.innerText = '';
+          outputElement.innerText = '';
+       } else if (keyType === '=') {
+          try {
+             const result = eval(inputElement.innerText);
+             outputElement.innerText = result;
+          } catch (error) {
+             outputElement.innerText = 'Error';
+          }
+       } else {
+          // Handle multiplication separately
+          if (keyType === '*') {
+             inputElement.innerText += '*';
+          } else {
+             inputElement.innerText += keyText;
+          }
+       }
+    });
+ });
